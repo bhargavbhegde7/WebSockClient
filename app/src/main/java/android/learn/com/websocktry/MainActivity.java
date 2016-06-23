2a4aff3c3e780d39019e2d61b3c313b128e8785f
bhargav <bhargavbhegde7@gmail.com>
2016-06-23 23:19:05 +0530

    voice recognition and auto send implmentation

--- app/src/main/java/android/learn/com/websocktry/MainActivity.java
+++ app/src/main/java/android/learn/com/websocktry/MainActivity.java
@@ -1,23 +1,35 @@
 package android.learn.com.websocktry;
 
+import android.content.ActivityNotFoundException;
+import android.content.Intent;
+import android.speech.RecognizerIntent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.TextUtils;
 import android.view.View;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.github.nkzawa.emitter.Emitter;
 import com.github.nkzawa.socketio.client.IO;
 import com.github.nkzawa.socketio.client.Socket;
 
 import java.net.URISyntaxException;
+import java.util.ArrayList;
+import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity{
+
+    private final int REQ_CODE_SPEECH_INPUT = 100;
+    private boolean AUTO_SEND = true;
 
     private EditText inputMessage;
     private TextView response;
     private TextView connectionState;
+    private TextView requestMessage;
 
     private Socket mSocket;
     {
@@ -35,6 +47,19 @@ public class MainActivity extends AppCompatActivity{
         inputMessage = (EditText) findViewById(R.id.messageInput);
         response = (TextView) findViewById(R.id.responseText);
         connectionState = (TextView) findViewById(R.id.connectionState);
+        requestMessage = (TextView) findViewById(R.id.requestMessage);
+
+        CheckBox autoSend = (CheckBox) findViewById(R.id.autoSend);
+        autoSend.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+                                                @Override
+                                                public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                                                    if(isChecked)
+                                                        AUTO_SEND = true;
+                                                    else
+                                                        AUTO_SEND = false;
+                                                }
+                                            }
+        );
     }
 
     private void addToView(String message){
@@ -90,6 +115,7 @@ public class MainActivity extends AppCompatActivity{
         try {
             //emit the 'new-message' event
             mSocket.emit("new-message", message);
+            requestMessage.setText(message);
         }catch(Exception e){
             System.out.println("Exception : "+e.getMessage());
         }
@@ -113,6 +139,14 @@ public class MainActivity extends AppCompatActivity{
         attemptSend(message);
     }
 
+    public void onMicClicked(View view){
+        try {
+            promptSpeechInput();
+        }catch(Exception e){
+            System.out.println("Exception : "+e.getMessage());
+        }
+    }
+
     @Override
     protected void onDestroy() {
         super.onDestroy();
@@ -124,5 +158,46 @@ public class MainActivity extends AppCompatActivity{
             System.out.println("Exception : "+e.getMessage());
         }
     }
+
+    /* for speech input */
+    /* Showing google speech input dialog */
+    private void promptSpeechInput() {
+        Intent intent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
+        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL,
+                RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
+        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE, Locale.getDefault());
+        intent.putExtra(RecognizerIntent.EXTRA_PROMPT,
+                "speak the words");
+        try {
+            startActivityForResult(intent, REQ_CODE_SPEECH_INPUT);
+        } catch (ActivityNotFoundException a) {
+            Toast.makeText(getApplicationContext(),
+                    "speech not supported",
+                    Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    /* Receiving speech input */
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        switch (requestCode) {
+            case REQ_CODE_SPEECH_INPUT: {
+                if (resultCode == RESULT_OK && null != data) {
+
+                    ArrayList<String> result = data
+                            .getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS);
+
+                    String outputText = result.get(0);
+                    if(AUTO_SEND) attemptSend(outputText);
+                    else inputMessage.setText(outputText);
+
+                }
+                break;
+            }
+        }
+    }
+    /* for speech input */
 
 }
